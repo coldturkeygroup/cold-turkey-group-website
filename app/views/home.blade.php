d907dea3c1d9838e46ef499e1b7fa5d2bbf68470
Aaron Huisinga <aaron@coldturkeygroup.com>
2015-03-20 20:54:28 -0500

    Trim unused code and update readme file

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,34 +1,61 @@
 @extends('layouts.home')
 
 @section('content')
-<div class="sections-wrapper">
-  <section id="why" class="section why">
+  <body class="home-page">
+  <header id="header" class="header">
     <div class="container">
-      <h2 class="title text-center">How Can Velocity Help You?</h2>
-      <p class="intro text-center">We take care of the UX and front-end design so you can save time building your site</p>
-      <div class="row item last-item">
-        <div class="content col-md-4 col-sm-12 col-xs-12">
-          <h3 class="title">Save you time and effort</h3>
-          <div class="desc">
-            <p>Explain one of your product benefits here. Let users know how they can benefit using your product. It’s also a good idea to back it up with a testimonial or tweet from your users.</p>
-            <p>The original PSD of the graphic is included in the package. You can easily customise the PSD to meet your needs.</p>
-          </div>
-          <div class="quote">
-            <div class="quote-profile">
-              <img class="img-responsive img-circle" src="assets/images/people/profile-s-1.png" alt="" />
-            </div><!--//profile-->
-            <div class="quote-content">
-              <blockquote><p><a href="https://twitter.com/3rdwave_themes" target="_blank">@velocity</a> Love it! Thank you for making my life easier and saving me time! I’ll definitely recommend it to my friends. :)</p></blockquote>
-              <p class="source">@LisaW, Bristol</p>
-            </div><!--//quote-content-->
-          </div><!--//quote-->
-        </div><!--//content-->
-        <figure class="figure col-md-7 col-sm-12 col-xs-12 col-md-offset-1 col-sm-offset-0 col-xs-offset-0">
-          <img class="img-responsive" src="assets/images/figures/figure-1.png" alt="" />
-          <figcaption class="figure-caption">(Screenshot: Coral - App &amp; website startup kit) </figcaption>
-        </figure>
-      </div><!--//item-->
-    </div><!--//container-->
-  </section><!--//why-->
-</div><!--//section-wrapper-->
+      <h1 class="logo">
+        <a href="index.html"><span class="text"><img id="ctg-logo" src="/assets/img/logo-white.png" alt="Cold Turkey Group logo" height="50px" style="height:50px"></span></a>
+      </h1>
+      <nav class="main-nav navbar-right" role="navigation">
+        <div class="navbar-header">
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar-collapse" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active nav-item"><a href="/">Home</a></li>
+            <li class="nav-item dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#">Products
+                <i class="fa fa-angle-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a target="_blank" href="https://tryfrontdesk.com/">FrontDesk</a></li>
+                <li><a target="_blank" href="http://platform.marketing/">Platform</a></li>
+              </ul>
+            </li>
+            <li class="nav-item"><a href="mailto:aaron@coldturkeygroup.com">Contact Us</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <div class="bg-slider-wrapper">
+    <div class="flexslider bg-slider">
+      <ul class="slides">
+        <li class="slide slide-1"></li>
+        <li class="slide slide-2"></li>
+      </ul>
+    </div>
+  </div>
+
+  <section class="promo section section-on-bg">
+    <div class="container text-center">
+      <h2 class="title">We build software to help your small business find more customers and make more money.</h2>
+
+      <p class="intro">The Cold Turkey Group is dedicated to helping your small business by using cutting edge
+        technology to find the right customers for you at a fraction of the cost.</p>
+
+      <p>
+        <a class="btn btn-cta btn-cta-primary fd" style="margin-right:50px" href="https://tryfrontdesk.com"><img src="/assets/img/frontdesk-logo.png">
+          FrontDesk</a>
+        <a class="btn btn-cta btn-cta-primary platform" href="http://platform.marketing"><img src="/assets/img/platform-logo.png">
+          Platform</a>
+      </p>
+    </div>
+  </section>
 @stop
